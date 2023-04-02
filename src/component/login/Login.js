7ef22dcf27e2aaa8e23b12c6567976f540980926
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-04-02 10:44:51 +0530

    Login useState Ready

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import './Login.css';
 const Login = () => {
+    const[user,SetUser]=useState({
+        email:'',
+        password:'',
+    })
+    const handleChange =(event)=>{
+        const{name,value}=event.target;
+        SetUser({
+            ...user,[name]:value
+        })
+    }
   return (
+    
     <div className='login'>
+     {console.log(user)};
         <h1>Login</h1>
-        <input type="text" placeholder='Enter your Email'/>
-        <input type="password" placeholder='Enter your Password'/>
+        <input type="text" name="email" value={user.name} placeholder='Enter your Email' onChange={handleChange}/>
+        <input type="password" name="password" value={user.password} placeholder='Enter your Password' onChange={handleChange}/>
         <div className='button'>Login</div>
         <div>or</div>
         <div className='button'>Register</div>
