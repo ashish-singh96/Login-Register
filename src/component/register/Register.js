a40ab40299e6ad380ca2a50fa71f3b06379eff3f
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-04-02 10:36:15 +0530

    Register useState Ready

--- src/component/register/Register.js
+++ src/component/register/Register.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Register.css';
 const Register = () => {
+    const[user,setUser]=useState({
+        name:'',
+        email:'',
+        password:'',
+        reEnterPassword:'',
+
+    })
+
+    const handleChange =(event)=>{
+        const{name,value}=event.target;
+        setUser({
+            ...user,[name]:value;
+        })
+    }
   return (
     <div className='register'>
     <h1>Register</h1>
-    <input type="text" placeholder='Your Name'/>
-    <input type="text" placeholder='Your Email'/>
-    <input type="password" placeholder='Enter Password'/>
-    <input type="password" placeholder='Re-enter Password'/>
+    <input type="text" name="name" value={user.name} placeholder='Your Name' onChange={handleChange}/>
+    <input type="text"  name="email" value={user.email} placeholder='Your Email' onChange={handleChange}/>
+    <input type="password"  name="password" value={user.password} placeholder='Enter Password' onChange={handleChange}/>
+    <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder='Re-enter Password' onChange={handleChange}/>
     <div className='button'>Register</div>
     <div>or</div>
     <div className='button'>Login</div>
