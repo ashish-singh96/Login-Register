14585e2a007ba4af11682a07743bfd78993e04c7
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-04-02 10:25:02 +0530

    Register Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Homepage from './component/homepage/Homepage';
 import './App.css';
 import Login from './component/login/Login';
+import Register from './component/register/Register';
 
 function App() {
   return (
     <div className='App'>
           {/* <Homepage/> */}
-          <Login/>
+          {/* <Login/> */}
+          <Register/>
     </div>
   );
 }
