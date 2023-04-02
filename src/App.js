0cd2301a7f1b20c01798417695c1f322f5d85e55
ashish-singh96 <ashishbhadauriya5344@gmail.com>
2023-04-02 10:18:08 +0530

    login component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Homepage from './component/homepage/Homepage';
+import './App.css';
+import Login from './component/login/Login';
+
+function App() {
+  return (
+    <div className='App'>
+          {/* <Homepage/> */}
+          <Login/>
+    </div>
+  );
+}
+
+export default App;
